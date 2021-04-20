2523bb905e66750ce7afebefb4a73e9f5bed2096
akulbii <alk@astarios.com>
2021-04-20 03:01:17 +0300

    Add logging to composer. Update composer to handle multiple body contents for single revision

--- src/Composer/ConfluenceComposer.php
+++ src/Composer/ConfluenceComposer.php
@@ -7,14 +7,21 @@ namespace HalloWelt\MigrateConfluence\Composer;
 use HalloWelt\MediaWiki\Lib\MediaWikiXML\Builder;
 use HalloWelt\MediaWiki\Lib\Migration\ComposerBase;
 use HalloWelt\MediaWiki\Lib\Migration\DataBuckets;
+use HalloWelt\MediaWiki\Lib\Migration\IOutputAwareInterface;
 use HalloWelt\MediaWiki\Lib\Migration\Workspace;
+use Symfony\Component\Console\Output\Output;
 
-class ConfluenceComposer extends ComposerBase {
+class ConfluenceComposer extends ComposerBase implements IOutputAwareInterface {
 
 	/**
 	 * @var DataBuckets
 	 */
 	private $dataBuckets;
+
+	/**
+	 * @var Output
+	 */
+	private $output = null;
 
 	/**
 	 * @param array $config
@@ -33,30 +40,52 @@ class ConfluenceComposer extends ComposerBase {
 		$this->dataBuckets->loadFromWorkspace( $this->workspace );
 	}
 
+	/**
+	 * @param Output $output
+	 */
+	public function setOutput( Output $output ) {
+		$this->output = $output;
+	}
+
 	public function buildXML( Builder $builder ) {
 		$pagesRevisions = $this->dataBuckets->getBucketData( 'title-revisions' );
 		$filesMap = $this->dataBuckets->getBucketData( 'files' );
 		$pageAttachmentsMap = $this->dataBuckets->getBucketData( 'title-attachments' );
 
 		foreach( $pagesRevisions as $pageTitle => $pageRevision ) {
+			$this->output->writeln("Processing: $pageTitle\n");
+
 			$pageRevisionData = explode( '@', $pageRevision[0] );
 
-			$bodyContentId = $pageRevisionData[0];
 			$timestamp = explode( '-', $pageRevisionData[1] )[1];
 
-			$pageContent = $this->workspace->getConvertedContent( $bodyContentId );
+			$bodyContentIds = $pageRevisionData[0];
+			$bodyContentIdsArr = explode( '/', $bodyContentIds );
+
+			$pageContent = "";
+			foreach( $bodyContentIdsArr as $bodyContentId ) {
+				$this->output->writeln("Getting '$bodyContentId' body content...");
+
+				$pageContent .= $this->workspace->getConvertedContent( $bodyContentId ) . "\n";
+			}
 
 			$builder->addRevision( $pageTitle, $pageContent, $timestamp );
 
 			// Append attachments
 			if( !empty( $pageAttachmentsMap[$pageTitle] ) ) {
+				$this->output->writeln("\nPage has attachments. Adding them...\n");
+
 				$attachments = $pageAttachmentsMap[$pageTitle];
 				foreach( $attachments as $attachment ) {
+					$this->output->writeln("Attachment: $attachment");
 					if( isset( $filesMap[$attachment] ) ) {
 						$filePath = $filesMap[$attachment][0];
 						$attachmentContent = file_get_contents( $filePath );
 
 						$this->workspace->saveUploadFile( $attachment, $attachmentContent );
+					}
+					else {
+						$this->output->writeln("Attachment file was not found!");
 					}
 				}
 			}
