9b5f2c1959a578190593ecb18d2d508f8e4e70e2
rvogel <vogel@hallowelt.biz>
2022-01-21 17:07:52 +0100

    Fix embedding of `_default*` elments
    See https://github.com/hallowelt/migrate-confluence/issues/28

--- src/Composer/ConfluenceComposer.php
+++ src/Composer/ConfluenceComposer.php
@@ -7,6 +7,8 @@ use HalloWelt\MediaWiki\Lib\Migration\ComposerBase;
 use HalloWelt\MediaWiki\Lib\Migration\DataBuckets;
 use HalloWelt\MediaWiki\Lib\Migration\IOutputAwareInterface;
 use HalloWelt\MediaWiki\Lib\Migration\Workspace;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use Symfony\Component\Console\Output\Output;
 
 class ConfluenceComposer extends ComposerBase implements IOutputAwareInterface {
@@ -101,11 +103,17 @@ class ConfluenceComposer extends ComposerBase implements IOutputAwareInterface {
 	 * @return void
 	 */
 	private function appendDefaultPages( Builder $builder ) {
-		$path = pathinfo( __DIR__ . '/_defaultpages/' );
-		$dir = parse_url( $path['dirname'] );
-		$basepath = $dir['host'] . ':/' . $dir['path'];
-		$files = glob( "$basepath/*/*" );
-		foreach ( $files as $file ) {
+		$basepath = __DIR__ . '/_defaultpages/';
+		$files = new RecursiveIteratorIterator(
+			new RecursiveDirectoryIterator( $basepath ),
+			RecursiveIteratorIterator::LEAVES_ONLY
+		);
+
+		foreach ( $files as $fileObj ) {
+			if ( $fileObj->isDir() ) {
+				continue;
+			}
+			$file = $fileObj->getPathname();
 			$namespacePrefix = basename( dirname( $file ) );
 			$pageName = basename( $file );
 			$wikiPageName = "$namespacePrefix:$pageName";
@@ -119,11 +127,17 @@ class ConfluenceComposer extends ComposerBase implements IOutputAwareInterface {
 	 * @return void
 	 */
 	private function addDefaultFiles() {
-		$path = pathinfo( __DIR__ . '/_defaultfiles/' );
-		$dir = parse_url( $path['dirname'] );
-		$basepath = $dir['host'] . ':/' . $dir['path'];
-		$files = glob( "$basepath/*" );
-		foreach ( $files as $file ) {
+		$basepath = __DIR__ . '/_defaultfiles/';
+		$files = new RecursiveIteratorIterator(
+			new RecursiveDirectoryIterator( $basepath ),
+			RecursiveIteratorIterator::LEAVES_ONLY
+		);
+
+		foreach ( $files as $fileObj ) {
+			if ( $fileObj->isDir() ) {
+				continue;
+			}
+			$file = $fileObj->getPathname();
 			$fileName = basename( $file );
 			$data = file_get_contents( $file );
 
