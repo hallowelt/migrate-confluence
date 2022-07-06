1cc4121e863e7a96b7968310f8cc734b89572429
DvogelHallowelt <32163064+DvogelHallowelt@users.noreply.github.com>
2022-07-06 16:12:28 +0200

    Image processor (#64)
    * Add image processor
    
    * Add unit test
    
    * Remove tests for convertable entities
    
    * Add test for spaceId GENERAL

---
+++ src/Converter/Processor/Image.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace HalloWelt\MigrateConfluence\Converter\Processor;
+
+use DOMDocument;
+use DOMElement;
+use DOMNode;
+use HalloWelt\MigrateConfluence\Converter\IProcessor;
+use HalloWelt\MigrateConfluence\Utility\ConversionDataLookup;
+
+class Image implements IProcessor {
+
+	/**
+	 * @var ConversionDataLookup
+	 */
+	protected $dataLookup;
+
+	/**
+	 * @var int
+	 */
+	protected $currentSpaceId;
+
+	/**
+	 * @var string
+	 */
+	protected $rawPageTitle;
+
+	/**
+	 * @var boolean
+	 */
+	protected $nsFileRepoCompat = false;
+
+	/**
+	 * @param ConversionDataLookup $dataLookup
+	 * @param int $currentSpaceId
+	 * @param string $rawPageTitle
+	 * @param bool $nsFileRepoCompat
+	 */
+	public function __construct( ConversionDataLookup $dataLookup,
+		int $currentSpaceId, string $rawPageTitle, bool $nsFileRepoCompat = false ) {
+		$this->dataLookup = $dataLookup;
+		$this->currentSpaceId = $currentSpaceId;
+		$this->rawPageTitle = $rawPageTitle;
+		$this->nsFileRepoCompat = $nsFileRepoCompat;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function process( DOMDocument $dom ): void {
+		$imageNodes = $dom->getElementsByTagName( 'image' );
+
+		foreach ( $imageNodes as $imageNode ) {
+			$this->doProcessImage( $imageNode );
+		}
+	}
+
+	/**
+	 * @param DOMElement $node
+	 * @return void
+	 */
+	private function doProcessImage( $node ): void {
+		$replacementNode = $node->ownerDocument->createTextNode( '[[Category:Broken_image]]' );
+
+		if ( $node instanceof DOMElement ) {
+			foreach ( $node->childNodes as $childNode ) {
+				if ( $childNode->nodeName === 'ri:url' ) {
+					$replacementNode = $this->makeImageUrlReplacement( $childNode );
+				} elseif ( $childNode->nodeName === 'ri:attachment' ) {
+					$replacementNode = $this->makeImageAttachmentReplacement( $childNode );
+				}
+			}
+		}
+
+		$node->parentNode->replaceChild(
+			$replacementNode,
+			$node
+		);
+	}
+
+	/**
+	 * @param DOMNode $node
+	 * @return array
+	 */
+	private function getImageAttributes( $node ): array {
+		$attributes = [];
+
+		$width = $node->getAttribute( 'ac:width' );
+		$height = $node->getAttribute( 'ac:height' );
+		if ( $width !== '' || $height !== '' ) {
+			$dimensions = 'px';
+			if ( $height !== '' ) {
+				$dimensions = 'x' . $height . $dimensions;
+				$attributes['height'] = $height;
+			}
+			$dimensions = $width . $dimensions;
+			$params[] = $dimensions;
+			if ( $width !== '' ) {
+				$attributes['width'] = $width;
+			}
+		}
+
+		if ( $node->getAttribute( 'ac:class' ) !== '' ) {
+			$attributes['class'][] = $node->getAttribute( 'ac:class' );
+		}
+		if ( $node->getAttribute( 'ac:thumbnail' ) !== '' ) {
+			$params[] = 'thumb';
+			$attributes['class'][] = 'thumb';
+		}
+		if ( !empty( $attributes['class'] ) ) {
+			$attributes['class'] = implode( ' ', $attributes['class'] );
+		}
+
+		if ( $node->getAttribute( 'ac:align' ) !== '' ) {
+			$params[] = $node->getAttribute( 'ac:align' );
+			$attributes['align'] = $node->getAttribute( 'ac:align' );
+		}
+
+		if ( $node->getAttribute( 'ac:alt' ) !== '' ) {
+			$attributes['alt'] = $node->getAttribute( 'ac:alt' );
+		}
+
+		return $attributes;
+	}
+
+	/**
+	 * @param DOMNode $node
+	 * @return array
+	 */
+	private function getImageParams( $node ): array {
+		$params = [];
+
+		$width = $node->getAttribute( 'ac:width' );
+		$height = $node->getAttribute( 'ac:height' );
+		if ( $width !== '' || $height !== '' ) {
+			$dimensions = 'px';
+			if ( $height !== '' ) {
+				$dimensions = 'x' . $height . $dimensions;
+			}
+			$dimensions = $width . $dimensions;
+			$params[] = $dimensions;
+		}
+
+		if ( $node->getAttribute( 'ac:thumbnail' ) !== '' ) {
+			$params[] = 'thumb';
+		}
+
+		if ( $node->getAttribute( 'ac:align' ) !== '' ) {
+			$params[] = $node->getAttribute( 'ac:align' );
+		}
+
+		return $params;
+	}
+
+	/**
+	 * @param DOMNode $node
+	 * @return DOMNode
+	 */
+	private function makeImageUrlReplacement( $node ): DOMNode {
+		$attributes = $this->getImageAttributes( $node->parentNode );
+		$attributes['src'] = $node->getAttribute( 'ri:value' );
+
+		$replacementNode = $node->ownerDocument->createElement( 'img' );
+
+		foreach ( $attributes as $name => $value ) {
+			$replacementNode->setAttribute( $name, $value );
+		}
+
+		return $replacementNode;
+	}
+
+	/**
+	 * @param DOMNode $node
+	 * @return DOMNode
+	 */
+	private function makeImageAttachmentReplacement( $node ): DOMNode {
+		$params = $this->getImageParams( $node->parentNode );
+
+		$filename = $node->getAttribute( 'ri:filename' );
+		$pageEl = $node->getElementsByTagName( 'page' )->item( 0 );
+
+		$rawPageTitle = $this->rawPageTitle;
+		$spaceId = $this->currentSpaceId;
+		if ( $pageEl instanceof DOMElement ) {
+			$rawPageTitle = $pageEl->getAttribute( 'ri:content-title' );
+			$spaceKey = $pageEl->getAttribute( 'ri:space-key' );
+			if ( !empty( $spaceKey ) ) {
+				$spaceId = $this->dataLookup->getSpaceIdFromSpaceKey( $spaceKey );
+			}
+		}
+
+		$rawPageTitle = basename( $rawPageTitle );
+		$confluenceFileKey = "$spaceId---$rawPageTitle---$filename";
+		$targetFilename = $this->dataLookup->getTargetFileTitleFromConfluenceFileKey( $confluenceFileKey );
+		array_unshift( $params, $targetFilename );
+		$replacementNode = $this->makeImageLinkWithDebugInfo( $node->ownerDocument, $params, $confluenceFileKey );
+
+		return $replacementNode;
+	}
+
+	/**
+	 * @param DOMDocument $dom
+	 * @param array $params
+	 * @return DOMNode
+	 */
+	public function makeImageLink( DOMDocument $dom, array $params ): DOMNode {
+		$params = array_map( 'trim', $params );
+
+		if ( $this->nsFileRepoCompat === true ) {
+			$params = $this->buildNsFileReopCompatParams( $params );
+		}
+
+		$replacementText = $this->getImageReplacement( $params );
+
+		return $dom->createTextNode( $replacementText );
+	}
+
+	/**
+	 * @param DOMDocument $dom
+	 * @param array $params
+	 * @param string $confluenceFileKey
+	 * @return DOMNode
+	 */
+	private function makeImageLinkWithDebugInfo( DOMDocument $dom, array $params, $confluenceFileKey ): DOMNode {
+		$params = array_map( 'trim', $params );
+
+		$debug = '';
+		if ( empty( $params ) || empty( $params[0] ) ) {
+			$debug = " ###BROKENIMAGE $confluenceFileKey ###";
+		} elseif ( $this->nsFileRepoCompat === true ) {
+			$params = $this->buildNsFileReopCompatParams( $params );
+		}
+
+		$replacementText = $this->getImageReplacement( $params );
+		$replacementText .= $debug;
+
+		return $dom->createTextNode( $replacementText );
+	}
+
+	/**
+	 * @param array $params
+	 * @return array
+	 */
+	private function buildNsFileReopCompatParams( $params ): array {
+		$filename = $params[0];
+
+		$filenameParts = explode( '_', $filename );
+		if ( count( $filenameParts ) > 2 ) {
+			$namespace = array_shift( $filenameParts );
+			$params[0] = "$namespace:";
+			$params[0] .= implode( '_', $filenameParts );
+		}
+
+		return $params;
+	}
+
+	/**
+	 * @param array $params
+	 * @return string
+	 */
+	private function getImageReplacement( $params ): string {
+		return '[[File:' . implode( '|', $params ) . ']]';
+	}
+}
---
+++ tests/phpunit/Converter/Processor/ImageTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace HalloWelt\MigrateConfluence\Tests\Converter\Processor;
+
+use DOMDocument;
+use HalloWelt\MigrateConfluence\Converter\Processor\Image;
+use HalloWelt\MigrateConfluence\Utility\ConversionDataLookup;
+use PHPUnit\Framework\TestCase;
+
+class ImageTest extends TestCase {
+
+	/**
+	 * @var ConversionDataLookup
+	 */
+	private $dataLookup;
+
+	/**
+	 * @var string
+	 */
+	private $dir = '';
+
+	/**
+	 * @covers HalloWelt\MigrateConfluence\Converter\Processor\Image::preprocess
+	 * @return void
+	 */
+	public function testPreprocess() {
+		$this->dir = dirname( dirname( __DIR__ ) ) . '/data';
+
+		$this->dataLookup = new ConversionDataLookup(
+			[
+				42 => 'ABC',
+				23 => 'DEVOPS'
+			],
+			[],
+			[
+				'0---SomePage---SomeImage2.png' => 'SomePage_SomeImage2.png',
+				'23---SomePage---SomeImage2.png' => 'DEVOPS_SomePage_SomeImage2.png'
+			],
+			[],
+			[]
+		);
+
+		/** SpaceId GENERAL */
+		$this->doTest( 'image-attachment-input-1.xml', 'image-attachment-output-1-general.xml', 0, 'SomePage' );
+		$this->doTest( 'image-attachment-input-2.xml', 'image-attachment-output-2.xml', 0, 'SomePage' );
+		$this->doTest( 'image-attachment-input-1.xml', 'image-attachment-output-3-general.xml', 0, 'SomePage', true );
+
+		/** Random SpaceId */
+		$this->doTest( 'image-attachment-input-1.xml', 'image-attachment-output-1.xml', 23, 'SomePage' );
+		$this->doTest( 'image-attachment-input-2.xml', 'image-attachment-output-2.xml', 23, 'SomePage' );
+		$this->doTest( 'image-attachment-input-1.xml', 'image-attachment-output-3.xml', 23, 'SomePage', true );
+	}
+
+	/**
+	 * @param string $input
+	 * @param string $output
+	 * @param bool $extNSFileRepo
+	 * @return void
+	 */
+	private function doTest( $input, $output, $spaceId, $rawPageTitle, $extNSFileRepo = false ): void {
+		$input = file_get_contents( "$this->dir/$input" );
+
+		$dom = new DOMDocument();
+		$dom->loadXML( $input );
+
+		$processor = new Image( $this->dataLookup, $spaceId, $rawPageTitle, $extNSFileRepo );
+		$processor->process( $dom );
+
+		$actualOutput = $dom->saveXML( $dom->documentElement );
+		$expectedOutput = file_get_contents( "$this->dir/$output" );
+
+		$this->assertEquals( $expectedOutput, $actualOutput );
+	}
+
+}
