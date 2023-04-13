691f4737db2264202d2fb0e9fdc850655024e66b
dvogel <daniel.vogel@hallowelt.com>
2023-04-13 10:03:08 +0200

    Remove data-params again and call in template in any case.

--- src/Converter/Processor/StructuredMacroChildren.php
+++ src/Converter/Processor/StructuredMacroChildren.php
@@ -52,36 +52,33 @@ class StructuredMacroChildren extends StructuredMacroProcessorBase {
 			}
 			$params[$name] = $paramNode->nodeValue;
 		}
+	
+		$templateParams = '';
+		foreach ( $params as $key => $value ) {
+			$templateParams .= '|' . $key . '=' . $value;
+		}
 
 		// https://github.com/JeroenDeDauw/SubPageList/blob/master/doc/USAGE.md
 		$div = $node->ownerDocument->createElement( 'div' );
 		if ( $depth !== false ) {
 			$div->setAttribute( 'class', 'subpagelist subpagelist-depth-' . $depth );
-
-			$templateParams = '';
-			foreach ( $params as $key => $value ) {
-				$templateParams .= '|' . $key . '=' . $value;
-			}
-
-			$div->appendChild(
-				$node->ownerDocument->createTextNode(
-					'{{SubpageList' . $templateParams . '}}'
-				)
-			);
-			$node->parentNode->replaceChild( $div, $node );
 		} else {
 			$div->setAttribute( 'class', 'subpagelist no-depth' );
-			if ( !empty( $params ) ) {
-				$div->setAttribute( 'data-params', json_encode( $params ) );
-			} else {
-				$div->setAttribute( 'data-params', '{}' );
-			}
+		}
+		$div->appendChild(
+			$node->ownerDocument->createTextNode(
+				'{{SubpageList' . $templateParams . '}}'
+			)
+		);
+
+		if ( empty( $templateParams ) ) {
 			$div->appendChild(
 				$node->ownerDocument->createTextNode(
 					'[[Category:Broken_macro/' . $this->getMacroName() . ']]'
 				)
 			);
-			$node->parentNode->replaceChild( $div, $node );
 		}
+
+		$node->parentNode->replaceChild( $div, $node );
 	}
 }
