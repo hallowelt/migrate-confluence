21cd04f18945b01c04803b2e8d9d71b011fffe1f
DvogelHallowelt <32163064+DvogelHallowelt@users.noreply.github.com>
2023-05-03 10:41:04 +0200

    Add structured macro processor for contentbylabel (#73)
    * Add structured macro processor for contentbylabel
    
    ERM27235
    
    * Add cql parser
    
    * Fix phpunit test for ContentByLabel macro
    
    * Add some comments
    
    * CC
    
    * CC
    
    * CC

---
+++ src/Converter/Processor/StructuredMacroContenByLabel.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace HalloWelt\MigrateConfluence\Converter\Processor;
+
+use HalloWelt\MigrateConfluence\Utility\CQLParser;
+
+class StructuredMacroContenByLabel extends StructuredMacroProcessorBase {
+
+	/**
+	 * @var string
+	 */
+	private $currentPageTitle = '';
+
+	/**
+	 * @param string $currentPageTitle
+	 */
+	public function __construct( string $currentPageTitle ) {
+		$this->currentPageTitle = $currentPageTitle;
+	}
+
+	/**
+	 *
+	 * @inheritDoc
+	 */
+	public function getMacroName(): string {
+		return 'contentbylabel';
+	}
+
+	/**
+	 * @param DOMNode $node
+	 * @return void
+	 */
+	protected function doProcessMacro( $node ): void {
+		$paramNodes = [];
+		foreach ( $node->childNodes as $childNode ) {
+			if ( $childNode->nodeName === 'ac:parameter' ) {
+				$paramNodes[] = $childNode;
+			}
+		}
+
+		$params = [];
+		foreach ( $paramNodes as $paramNode ) {
+			if ( !$paramNode->hasAttributes() ) {
+				continue;
+			}
+			$name = $paramNode->getAttribute( 'ac:name' );
+			if ( $name === 'page' ) {
+				$params[$name] = $this->currentPageTitle;
+				continue;
+			}
+			$params[$name] = $paramNode->nodeValue;
+		}
+
+		if ( isset( $params['cql'] ) ) {
+			$params['conditions'] = $this->getConditionsForCQL( $params['cql'] );
+		}
+
+		$templateParams = '';
+		foreach ( $params as $key => $value ) {
+			$templateParams .= "|$key=$value\n";
+		}
+
+		// https://github.com/JeroenDeDauw/SubPageList/blob/master/doc/USAGE.md
+		$text = $node->ownerDocument->createTextNode( "{{ContenByLabel\n$templateParams}}" );
+
+		$node->parentNode->replaceChild( $text, $node );
+	}
+
+	/**
+	 * @param string $cql
+	 * @return string
+	 */
+	private function getConditionsForCQL( string $cql ): string {
+		$cqlParser = new CQLParser();
+		$conditions = $cqlParser->parse( $cql );
+
+		return $conditions;
+	}
+}
---
+++ tests/phpunit/Utility/CQLParser/CQLParserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace HalloWelt\MigrateConfluence\Tests\Utility\CQLParser;
+
+use HalloWelt\MigrateConfluence\Utility\CQLParser;
+use PHPUnit\Framework\TestCase;
+
+class CQLParserTest extends TestCase {
+	/**
+	 * @covers \HalloWelt\MigrateConfluence\Utility\CQLParser::parse()
+	 */
+	public function testParse() {
+		$cqlParser = new CQLParser();
+
+		$input = 'label = "label_1" and label = "label_2"';
+		$expected = '[[Category:Label_1]][[Category:Label_2]]';
+
+		$actual = $cqlParser->parse( $input );
+
+		$this->assertEquals( $expected, $actual );
+	}
+}
