c7ceffba1946a6a4e328a3760febd22113d3cc10
Daniel Vogel <daniel.vogel@hallowelt.com>
2025-04-22 13:20:27 +0200

    Inprove code macro to protect html entities from pandoc conversion
    ERM27235

--- src/Converter/Postprocessor/RestoreCode.php
+++ src/Converter/Postprocessor/RestoreCode.php
@@ -10,12 +10,46 @@ class RestoreCode implements IPostprocessor {
 	 * @inheritDoc
 	 */
 	public function postprocess( string $wikiText ): string {
-		$newWikiText = preg_replace(
-			'#<pre class="PRESERVESYNTAXHIGHLIGHT"(.*?)>(.*?)</pre>#si',
-			'<syntaxhighlight$1>$2</syntaxhighlight>',
+		var_dump( $wikiText );
+		$wikiText = preg_replace_callback(
+			'#<pre class="PRESERVESYNTAXHIGHLIGHT"(.*?)>(.*?)</pre>#si', function( $matches ) {
+				$attribs = $this->getAttributes( $matches[1] );
+
+				if ( isset( $attribs['data-broken-macro'] ) ) {
+					$code = '[[Category:' . $attribs['data-broken-macro'] . ']]';
+					unset( $attribs['data-broken-macro'] );
+					$params = $this->buildAttributes( $attribs );
+					return '<syntaxhighlight' . $params . '></syntaxhighlight>' . $code;
+				} else {
+					$code = base64_decode( $matches[2] );
+				}
+
+				return '<syntaxhighlight' . $matches[1] . '>' . $code . '</syntaxhighlight>';
+			},
 			$wikiText
 		);
 
-		return $newWikiText;
+		return $wikiText;
+	}
+
+	private function getAttributes( $params ) {
+		$matches = [];
+		preg_match_all( '#\s(.*?)="(.*?)"#', $params, $matches );
+
+		$attribs = [];
+		for ( $index = 0; $index < count( $matches[1] ); $index++ ) {
+			$name = $matches[1][$index];
+			$attribs[$name] = $matches[2][$index];
+		}
+
+		return $attribs;
+	}
+
+	private function buildAttributes( $attribs ) {
+		$params = '';
+		foreach ( $attribs as $key => $value ) {
+			$params .= ' ' . $key . '="' . $value . '"';
+		}
+		return $params;
 	}
 }
