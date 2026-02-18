843a0ce95e74a8eff613d9e88cc7f037aebfb0ce
DvogelHallowelt <32163064+DvogelHallowelt@users.noreply.github.com>
2026-02-18 14:24:25 +0100

    Improve details macro (#141)
    * Fix details macro and add a fix for multiline macros
    
    * CC
    
    * Update ConfluenceConverter with new post processor

---
+++ src/Converter/Postprocessor/FixMultilineTemplate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HalloWelt\MigrateConfluence\Converter\Postprocessor;
+
+use HalloWelt\MigrateConfluence\Converter\IPostprocessor;
+
+class FixMultilineTemplate implements IPostprocessor {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function postprocess( string $wikiText ): string {
+		$wikiText = preg_replace_callback(
+			'/\{\{(.*?)\}\}/s',
+			static function ( $match ) {
+				$lines = explode( "###BREAK###", $match[0] );
+				for ( $index = 0; $index < count( $lines ); $index++ ) {
+					$line = $lines[$index];
+					if ( strpos( $line, ' ' ) === 0 ) {
+						$lines[$index] = substr( $line, 1 );
+					}
+				}
+				return implode( "###BREAK###", $lines );
+			},
+			$wikiText
+		);
+
+		return $wikiText;
+	}
+}
