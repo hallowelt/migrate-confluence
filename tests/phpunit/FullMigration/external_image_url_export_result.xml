hallowelt/migrate-confluence

commit 5870d62211aad1765daccccf2d021405bf5a3eff

Fix external image (#173)

src/Converter/Processor/Image.php
@@ -189,18 +189,24 @@ class Image implements IProcessor {
 	}
 
 	/**
+	 * MediaWiki does not render an img tag.
+	 * But with $wgAllowExternalImages it can show external images.
+	 * If this varaiable is false we show at least the url as link.
+	 *
 	 * @param DOMElement $node
 	 * @return DOMNode
 	 */
 	private function makeImageUrlReplacement( $node ): DOMNode {
 		$attributes = $this->getImageAttributes( $node->parentNode );
-		$attributes['src'] = $node->getAttribute( 'ri:value' );
+		$src = $node->getAttribute( 'ri:value' );
 
-		$replacementNode = $node->ownerDocument->createElement( 'img' );
+		$replacementNode = $node->ownerDocument->createElement( 'span' );
 
 		foreach ( $attributes as $name => $value ) {
 			$replacementNode->setAttribute( $name, $value );
 		}
+
+		$replacementNode->nodeValue = $src;
 
 		return $replacementNode;
 	}

tests/phpunit/data/image-attachment-input-2.xml
@@ -2,7 +2,7 @@
 	<div>
 		<ac:image ac:height="150">
 			<ri:url
-				ri:value="https://server/confluence/download/attachments/123456789/image2021-8-17_10-15-14.png?version=1&amp;modificationDate=1914846492120&amp;api=v2"
+				ri:value="https://server/confluence/download/attachments/123456789/image2021-8-17_10-15-14.png"
 			/>
 		</ac:image>
 	</div>

tests/phpunit/data/image-attachment-output-2.xml
@@ -1,5 +1,5 @@
 <xml xmlns:ac="sample_namespace" xmlns:ri="sample_second_namespace">
 	<div>
-		<img height="150" src="https://server/confluence/download/attachments/123456789/image2021-8-17_10-15-14.png?version=1&amp;modificationDate=1914846492120&amp;api=v2"/>
+		<span height="150">https://server/confluence/download/attachments/123456789/image2021-8-17_10-15-14.png</span>
 	</div>
 </xml>

commit c5b2692b78dc36ac6c028a34b574f138c051d220

Fix bug introduced with 5870d622 and introduce full migration tests

src/Converter/Processor/Image.php
@@ -199,7 +199,9 @@ class Image implements IProcessor {
 			$replacementNode->setAttribute( $name, $value );
 		}
 
-		$replacementNode->nodeValue = $src;
+		$replacementNode->appendChild(
+			$node->ownerDocument->createTextNode( $src )
+		);
 
 		return $replacementNode;
 	}

tests/phpunit/FullMigration/FullMigrationTest.php
@@ -1,0 +1,179 @@
+<?php
+
+namespace HalloWelt\MigrateConfluence\Tests\FullMigration;
+
+use HalloWelt\MediaWiki\Lib\MediaWikiXML\Builder;
+use HalloWelt\MediaWiki\Lib\Migration\DataBuckets;
+use HalloWelt\MediaWiki\Lib\Migration\Workspace;
+use HalloWelt\MigrateConfluence\Analyzer\ConfluenceAnalyzer;
+use HalloWelt\MigrateConfluence\Composer\ConfluenceComposer;
+use HalloWelt\MigrateConfluence\Converter\ConfluenceConverter;
+use HalloWelt\MigrateConfluence\Extractor\ConfluenceExtractor;
+use PHPUnit\Framework\TestCase;
+use SplFileInfo;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class FullMigrationTest extends TestCase {
+
+	/** @var string */
+	private $workDir;
+
+	protected function setUp(): void {
+		$this->workDir = sys_get_temp_dir() . '/confluence-migration-test-' . uniqid();
+		mkdir( $this->workDir, 0755, true );
+		mkdir( $this->workDir . '/result/images', 0755, true );
+	}
+
+	protected function tearDown(): void {
+		$this->removeDirectory( $this->workDir );
+	}
+
+	/**
+	 * @covers \HalloWelt\MigrateConfluence\Analyzer\ConfluenceAnalyzer
+	 * @covers \HalloWelt\MigrateConfluence\Extractor\ConfluenceExtractor
+	 * @covers \HalloWelt\MigrateConfluence\Converter\ConfluenceConverter
+	 * @covers \HalloWelt\MigrateConfluence\Composer\ConfluenceComposer
+	 */
+	public function testExternalImageUrlMigration(): void {
+		$sourceFile = __DIR__ . '/external_image_url_export_source.xml';
+		$expectedFile = __DIR__ . '/external_image_url_export_result.xml';
+
+		copy( $sourceFile, $this->workDir . '/entities.xml' );
+
+		$workspace = new Workspace( new SplFileInfo( $this->workDir ) );
+		$output = new BufferedOutput();
+		$config = [];
+
+		// Step 1: Analyze
+		$this->runAnalyze( $config, $workspace, $output );
+
+		// Step 2: Extract
+		$this->runExtract( $config, $workspace );
+
+		// Step 3: Convert
+		$this->runConvert( $config, $workspace, $output );
+
+		// Step 4: Compose
+		$this->runCompose( $config, $workspace, $output );
+
+		// Step 5: Verify
+		$actualFile = $this->workDir . '/result/pages.xml';
+		$this->assertFileExists( $actualFile );
+
+		$expectedXml = file_get_contents( $expectedFile );
+		$actualXml = file_get_contents( $actualFile );
+
+		$this->assertXmlStringEqualsXmlString( $expectedXml, $actualXml );
+	}
+
+	/**
+	 * @param array $config
+	 * @param Workspace $workspace
+	 * @param BufferedOutput $output
+	 */
+	private function runAnalyze( array $config, Workspace $workspace, BufferedOutput $output ): void {
+		$buckets = new DataBuckets( [
+			'global-files',
+		] );
+
+		$analyzer = new ConfluenceAnalyzer( $config, $workspace, $buckets );
+		$analyzer->setOutput( $output );
+		$analyzer->analyze( new SplFileInfo( $this->workDir . '/entities.xml' ) );
+
+		$buckets->saveToWorkspace( $workspace );
+	}
+
+	/**
+	 * @param array $config
+	 * @param Workspace $workspace
+	 */
+	private function runExtract( array $config, Workspace $workspace ): void {
+		$buckets = new DataBuckets( [
+			'global-title-metadata',
+			'global-attachment-metadata',
+			'global-revision-contents',
+			'global-body-content-id-to-page-id-map',
+			'global-body-content-id-to-space-description-id-map',
+		] );
+
+		$extractor = new ConfluenceExtractor( $config, $workspace, $buckets );
+		$extractor->extract( new SplFileInfo( $this->workDir . '/entities.xml' ) );
+
+		$buckets->saveToWorkspace( $workspace );
+	}
+
+	/**
+	 * @param array $config
+	 * @param Workspace $workspace
+	 * @param BufferedOutput $output
+	 */
+	private function runConvert( array $config, Workspace $workspace, BufferedOutput $output ): void {
+		$rawDir = $this->workDir . '/content/raw';
+		if ( !is_dir( $rawDir ) ) {
+			return;
+		}
+
+		$wikiDir = $this->workDir . '/content/wikitext';
+		if ( !is_dir( $wikiDir ) ) {
+			mkdir( $wikiDir, 0755, true );
+		}
+
+		$rawFiles = glob( $rawDir . '/*.mraw' );
+		foreach ( $rawFiles as $rawFilePath ) {
+			$converter = new ConfluenceConverter( $config, $workspace );
+			$converter->setOutput( $output );
+
+			$wikiText = $converter->convert( new SplFileInfo( $rawFilePath ) );
+
+			$id = basename( $rawFilePath, '.mraw' );
+			file_put_contents( $wikiDir . '/' . $id . '.wiki', $wikiText );
+		}
+	}
+
+	/**
+	 * @param array $config
+	 * @param Workspace $workspace
+	 * @param BufferedOutput $output
+	 */
+	private function runCompose( array $config, Workspace $workspace, BufferedOutput $output ): void {
+		$buckets = new DataBuckets( [
+			'global-space-id-homepages',
+			'global-space-id-to-description-id-map',
+			'global-body-content-id-to-space-description-id-map',
+			'global-body-content-id-to-page-id-map',
+			'global-title-attachments',
+			'global-title-revisions',
+			'global-files',
+			'global-additional-files',
+		] );
+		$buckets->loadFromWorkspace( $workspace );
+
+		$composer = new ConfluenceComposer( $config, $workspace, $buckets );
+		$composer->setOutput( $output );
+		$composer->setDestinationPath( $this->workDir );
+
+		$builder = new Builder();
+		$composer->buildXML( $builder );
+	}
+
+	/**
+	 * @param string $dir
+	 */
+	private function removeDirectory( string $dir ): void {
+		if ( !is_dir( $dir ) ) {
+			return;
+		}
+		$items = new \RecursiveIteratorIterator(
+			new \RecursiveDirectoryIterator( $dir, \RecursiveDirectoryIterator::SKIP_DOTS ),
+			\RecursiveIteratorIterator::CHILD_FIRST
+		);
+		foreach ( $items as $item ) {
+			if ( $item->isDir() ) {
+				rmdir( $item->getRealPath() );
+			} else {
+				unlink( $item->getRealPath() );
+			}
+		}
+		rmdir( $dir );
+	}
+}

tests/phpunit/FullMigration/external_image_url_export_result.xml
@@ -1,0 +1,389 @@
+<?xml version="1.0"?>
+<mediawiki>
+  <page>
+    <title>Template:SimplePanel.css</title>
+    <revision>
+      <model>sanitized-css</model>
+      <format>text/css</format>
+      <text>.simplepanel {
+	border: 2px solid;
+	border-radius: 3px;
+	padding: 0.5em;
+}
+
+.simplepanel.info {
+	border-color: #1c75ff;
+	background-color: #bdd7ff;
+}
+
+.simplepanel.note {
+	border-color: #ffb01c;
+	background-color: #ffe5b5;
+}
+
+.simplepanel.tip {
+	border-color: #53ac00;
+	background-color: #deffbf;
+}
+
+.simplepanel.warning {
+	border-color: #ff4e14;
+	background-color: #ffcab8;
+}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Excerpt</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{| class="mw-collapsible mw-collapsed wikitable"
+! {{{title|}}}
+|-
+|
+{{{body|}}}
+|}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:TableFilter</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;div class="macro-table-filter"&gt;
+	{{body|}}
+&lt;/div&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Panel</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;
+&lt;div class="panel"&gt;{{#if:{{{title|}}}|
+&lt;span class="panel__heading"&gt;'''{{{title|}}}'''&lt;/span&gt;&lt;br /&gt;
+|}}&lt;div class="panel__content"&gt;
+{{{body|Add a &lt;code&gt;body&lt;/code&gt; parameter}}}
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Info</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{{Textbox|boxtype=important|header={{{title|}}}|text={{{body|}}}|icon=yes}}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Layout.css</title>
+    <revision>
+      <model>sanitized-css</model>
+      <format>text/css</format>
+      <text>.ac-layout-section {
+	display: flex;
+}
+
+.ac-layout-section.three_equal .ac-layout-cell {
+	flex: 33%;
+}
+
+.ac-layout-section.two_equal .ac-layout-cell {
+	flex: 50%;
+}
+
+.ac-layout-section.three_with_sidebars .ac-layout-cell:nth-child(1) {
+	flex: 25%;
+}
+.ac-layout-section.three_with_sidebars .ac-layout-cell:nth-child(2) {
+	flex: 50%;
+}
+.ac-layout-section.three_with_sidebars .ac-layout-cell:nth-child(3) {
+	flex: 25%;
+}
+
+.ac-layout-section.two_right_sidebar .ac-layout-cell:nth-child(1) {
+	flex: 66%;
+}
+.ac-layout-section.two_right_sidebar .ac-layout-cell:nth-child(2) {
+	flex: 33%;
+}
+
+.ac-layout-section.two_left_sidebar .ac-layout-cell:nth-child(1) {
+	flex: 33%;
+}
+.ac-layout-section.two_left_sidebar .ac-layout-cell:nth-child(2) {
+	flex: 66%;
+}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:DetailsSummary</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{| class="mw-collapsible mw-collapsed wikitable"
+! {{{title|}}}
+|-
+|
+{{{body|}}}
+|}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Tip</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{{Textbox|boxtype=tip|header={{{title|}}}|text={{{body|}}}|icon=yes}}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Drawio</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{{#tag:drawio
+|
+|filename={{{diagramName}}}
+}}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Jira</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;noinclude&gt;Links to JIRA can be handled with "Interwiki-Links"
+&lt;/noinclude&gt;&lt;includeonly&gt;[[jira:{{{key}}}]]&lt;/includeonly&gt;
+</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:RecentlyUpdated</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;
+{{#ask:[[{{NAMESPACE}}:+]][[Modification date::+]]
+|format=ul
+|limit=10
+}}
+&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:ContentByLabel</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;noinclude&gt;This template is used to convert the structured macro "contentbylabel".&lt;/noinclude&gt;
+&lt;includeonly&gt;
+{{#ask:{{#if:{{{conditions|}}}|{{{conditions|}}}|[[Category:{{#arraymap:{{{labels|}}}|,|@@|@@|{{!}}{{!}}}}]]}}
+|format=ul
+|limit=999
+}}
+&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:SubpageList</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;{{#ask:[[-Subpage::{{{page|{{FULLPAGENAME}}}}}]]
+|format=template
+|template=SubpageListRow
+|userparam={{{page|{{FULLPAGENAME}}}}}
+|limit=9999
+|link=none
+}}&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Note</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{{Textbox|boxtype=note|header={{{title|}}}|text={{{body|}}}|icon=yes}}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:SubpageListRow</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;{{#ifeq:{{#titleparts: {{{1}}} | -1 }}|{{{#userparam}}}|*[[{{{1}}}|{{#titleparts: {{{1}}} | | -1 }}]]}}&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:InlineComment</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;&lt;span style="display:inline-block;padding:0.2em;background-color:yellow"&gt;{{{1|N/A}}}&lt;/span&gt;&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Details</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;div class="macro-details" {{#if:{{{id|}}}|data-id="{{{id|}}}"}} {{#if:{{{hidden|}}}|style="display:none;"}}&gt;
+	{{{body}}}
+&lt;/div&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Layout</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;&lt;templatestyles src="Template:Layout.css" /&gt;&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Status</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;&lt;span style="display:inline-block;padding:0.2em;font-variant-caps:all-small-caps;background-color:{{{colour|none}}}"&gt;{{{title|N/A}}}&lt;/span&gt;&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:ViewFile</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;noinclude&gt;Template for confluence macro "view-file"
+&lt;/noinclude&gt;&lt;includeonly&gt;[[File:{{{filename}}}]]&lt;/includeonly&gt;
+</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:ExcerptInclude</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{{:{{{target|}}}}}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Warning</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{{Textbox|boxtype=warning|header={{{title|}}}|text={{{body|}}}|icon=yes}}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:AttachmentsSectionStart</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;
+== Attachments ==
+&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:PageTree</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;{{#subpages:{{{content-title}}}}}&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:SimplePanel</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;includeonly&gt;&lt;templatestyles src="Template:SimplePanel.css" /&gt;
+&lt;div class="simplepanel {{{type|}}}" data-panel-type="{{{type|}}}"&gt;
+&lt;div class="simplepanel__icon"&gt;
+&lt;span role="img" aria-label="Panel {{{type|}}}"&gt;
+[[File:Icon-{{{type|info}}}.svg|link=]]
+&lt;/span&gt;
+&lt;/div&gt;{{#if:{{{title|}}}|
+&lt;span class="simplepanel__heading"&gt;'''{{{title|}}}'''&lt;/span&gt;&lt;br /&gt;
+|}}&lt;div class="simplepanel__content"&gt;
+{{{body|Add a &lt;code&gt;body&lt;/code&gt; parameter}}}
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/includeonly&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Expand</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{| class="mw-collapsible mw-collapsed wikitable"
+! {{{title|}}}
+|-
+|
+{{{body|}}}
+|}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:SpaceDetails</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;noinclude&gt;Template for macro "space-details"
+&lt;/noinclude&gt;&lt;includeonly&gt;{| class="wikitable" style="width:{{{width}}}"
+|-
+| Name || {{NAMESPACE}}
+|-
+| Description || {{#ask:[[{{NAMESPACE}}:+]][[Space description::+]]|?Space description=|mainlabel=-|link=none}}
+|-
+| Pages in namespace || {{#ask:[[{{NAMESPACE}}:+]]|format=count}}
+|-
+| Last page edit || {{#ask:[[{{NAMESPACE}}:+]]|limit=1|sort=Modification date|order=desc|searchlabel=}} 
+|-
+| Last modification date || {{#ask:[[{{NAMESPACE}}:+]]|?Modification date=|limit=1|sort=Modification date|order=desc|searchlabel=|mainlabel=-}} 
+|-
+| Last modifier || {{#ask:[[{{NAMESPACE}}:+]]|?Last editor is=|limit=1|sort=Modification date|order=desc|searchlabel=|mainlabel=-}} 
+|}&lt;/includeonly&gt;
+</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:Gliffy</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>{{#tag:drawio
+|
+|filename={{{name}}}
+}}</text>
+    </revision>
+  </page>
+  <page>
+    <title>Template:AttachmentsSectionEnd</title>
+    <revision>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;!-- Nothing to do here --&gt;</text>
+    </revision>
+  </page>
+  <page>
+    <title>CON:Test_Page</title>
+    <revision>
+      <timestamp>20200101000000</timestamp>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+      <text>&lt;span id="image-url"&gt;&lt;/span&gt;
+=== Image URL ===
+
+&lt;span class="confluence-embedded-file-wrapper"&gt;&lt;span&gt;https://example.com/download/attachments/12345678/test-image.png?version=1&amp;amp;modificationDate=1000000000000&amp;amp;api=v2&lt;/span&gt;&lt;/span&gt;
+ &lt;!-- From bodyContent 20000001.mraw --&gt;
+</text>
+    </revision>
+  </page>
+</mediawiki>

tests/phpunit/FullMigration/external_image_url_export_source.xml
@@ -1,0 +1,38 @@
+<hibernate-generic datetime="2020-01-01 00:00:00">
+
+    <object class="Space" package="com.atlassian.confluence.spaces">
+        <id name="id">10000000</id>
+        <property name="name">Confluence-Testbereich</property>
+        <property name="key">CON</property>
+        <property name="lowerKey">con</property>
+        <property name="creationDate">2020-01-01 00:00:00.000</property>
+        <property name="lastModificationDate">2020-01-01 00:00:00.000</property>
+        <property name="spaceType">global</property>
+        <property name="spaceStatus" enum-class="SpaceStatus" package="com.atlassian.confluence.spaces">CURRENT
+        </property>
+    </object>
+
+    <object class="Page" package="com.atlassian.confluence.pages">
+        <id name="id">10000001</id>
+        <property name="hibernateVersion">1</property>
+        <property name="title">Test Page</property>
+        <property name="lowerTitle">test page</property>
+        <property name="version">1</property>
+        <property name="creationDate">2020-01-01 00:00:00.000</property>
+        <property name="lastModificationDate">2020-01-01 00:00:00.000</property>
+        <property name="versionComment"></property>
+        <property name="contentStatus">current</property>
+        <property name="creator" class="ConfluenceUserImpl" package="com.atlassian.confluence.user"><id name="key">000000000000000000000000000000001</id></property>
+        <property name="lastModifier" class="ConfluenceUserImpl" package="com.atlassian.confluence.user"><id name="key">000000000000000000000000000000001</id></property>
+        <property name="space" class="Space" package="com.atlassian.confluence.spaces"><id name="id">10000000</id></property>
+    </object>
+
+    <object class="BodyContent" package="com.atlassian.confluence.core">
+        <id name="id">20000001</id>
+        <property name="body"><![CDATA[<h3>Image URL</h3><p><span class="confluence-embedded-file-wrapper"><ac:image ac:border="true"><ri:url ri:value="https://example.com/download/attachments/12345678/test-image.png?version=1&amp;modificationDate=1000000000000&amp;api=v2" /></ac:image></span></p>]]></property>
+        <property name="bodyType">2</property>
+        <property name="content" class="Page" package="com.atlassian.confluence.pages"><id name="id">10000001</id></property>
+    </object>
+
+</hibernate-generic>
+