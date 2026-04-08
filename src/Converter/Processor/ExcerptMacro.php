f6833f4685146154a27b4422115834f9ebdd5d18
driedmueller <daniel.riedmueller@hallowelt.com>
2026-04-08 10:48:02 +0200

    rewrite excerpt macro to support excerpt extension

--- src/Converter/Processor/ExcerptMacro.php
+++ src/Converter/Processor/ExcerptMacro.php
@@ -2,11 +2,15 @@
 
 namespace HalloWelt\MigrateConfluence\Converter\Processor;
 
+use DOMNode;
+
 /**
+ * Converts the Confluence excerpt macro to a BlueSpice <excerpt-block> element.
+ * The broken macro category is added because the BlueSpice Excerpt extension is not yet available.
+ *
  * @see https://confluence.atlassian.com/doc/excerpt-macro-148062.html
- * @see https://docs.atlassian.com/DAC/javadoc/confluence/4.0/reference/com/atlassian/confluence/macro/Macro.OutputType.html
  */
-class ExcerptMacro extends ConvertMacroToTemplateWithBodyBase {
+class ExcerptMacro extends StructuredMacroProcessorBase {
 
 	/**
 	 * @inheritDoc
@@ -18,14 +22,35 @@ class ExcerptMacro extends ConvertMacroToTemplateWithBodyBase {
 	/**
 	 * @inheritDoc
 	 */
-	protected function getWikiTextTemplateStartName(): string {
-		return 'ExcerptStart';
-	}
+	protected function doProcessMacro( DOMNode $node ): void {
+		$macroId = $node->getAttribute( 'ac:macro-id' );
+		$hidden = 'false';
 
-	/**
-	 * @inheritDoc
-	 */
-	protected function getWikiTextTemplateEndName(): string {
-		return 'ExcerptEnd';
+		foreach ( $node->childNodes as $childNode ) {
+			if ( $childNode->nodeName === 'ac:parameter'
+				&& $childNode->getAttribute( 'ac:name' ) === 'hidden' ) {
+				$hidden = trim( $childNode->nodeValue );
+				break;
+			}
+		}
+
+		$excerptBlock = $node->ownerDocument->createElement( 'excerpt-block' );
+		$excerptBlock->setAttribute( 'name', $macroId );
+		$excerptBlock->setAttribute( 'hidden', $hidden );
+
+		foreach ( $node->childNodes as $childNode ) {
+			if ( $childNode->nodeName === 'ac:rich-text-body' ) {
+				foreach ( iterator_to_array( $childNode->childNodes ) as $bodyChild ) {
+					$excerptBlock->appendChild( $bodyChild->cloneNode( true ) );
+				}
+			}
+		}
+
+		$node->parentNode->replaceChild( $excerptBlock, $node );
+
+		$brokenCategory = $excerptBlock->ownerDocument->createTextNode(
+			$this->getBrokenMacroCategory()
+		);
+		$excerptBlock->parentNode->insertBefore( $brokenCategory, $excerptBlock->nextSibling );
 	}
 }
