160b4c60babe1440217ea051066d09615fc53b92
driedmueller <daniel.riedmueller@hallowelt.com>
2026-05-20 09:59:07 +0200

    Added pages post content processor interface
    ERM46511

---
+++ src/Composer/Processor/TemplateContentPostProcessor.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace HalloWelt\MigrateConfluence\Composer\Processor;
+
+use HalloWelt\MigrateConfluence\Composer\IPageContentPostProcessor;
+
+class TemplateContentPostProcessor implements IPageContentPostProcessor {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function postProcess( string $pageTitle, string $pageContent ): string {
+		if ( strpos( $pageTitle, 'Template:' ) !== 0 ) {
+			return $pageContent;
+		}
+		// Skip default (non-migrated) templates
+		$defaultTemplates = $this->getDefaultTemplateNames();
+		if ( in_array( $pageTitle, $defaultTemplates ) ) {
+			return $pageContent;
+		}
+
+		return $pageContent . "\n<includeonly>{{#set: Created by macro=$1 }}</includeonly>\n";
+	}
+
+	/**
+	 * @return array
+	 */
+	private function getDefaultTemplateNames(): array {
+		$basepath = dirname( __DIR__ ) . '/_defaultpages/Template/';
+		$names = [];
+		if ( is_dir( $basepath ) ) {
+			foreach ( scandir( $basepath ) as $file ) {
+				if ( $file === '.' || $file === '..' ) {
+					continue;
+				}
+				$names[] = 'Template:' . $file;
+			}
+		}
+		return $names;
+	}
+}
