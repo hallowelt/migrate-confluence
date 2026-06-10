ca9de1b2eec8c3aba95e11a1200c140fb537a3c1
driedmueller <daniel.riedmueller@hallowelt.com>
2026-06-10 14:41:23 +0200

    Support page comments from blog posts
    ERM46627

--- src/Database/WorkspaceDB.php
+++ src/Database/WorkspaceDB.php
@@ -3299,10 +3299,12 @@ class WorkspaceDB {
 	public function getCommentsForPages(): array {
 		$transaction = $this->cachedPrepare(
 			'SELECT c.*, COALESCE(p.wiki_title, bp.wiki_title) AS wiki_title FROM comments c
-			LEFT JOIN pages p ON p.page_id = c.container_id AND c.content_class = \'Page\'
-			LEFT JOIN blog_posts bp ON bp.page_id = c.container_id AND c.content_class = \'BlogPost\'
-			WHERE c.content_class IN (\'Page\', \'BlogPost\')'
+			LEFT JOIN pages p ON p.page_id = c.container_id AND c.content_class = :class_page
+			LEFT JOIN blog_posts bp ON bp.page_id = c.container_id AND c.content_class = :class_blogpost
+			WHERE c.content_class IN (:class_page, :class_blogpost)'
 		);
+		$transaction->bindValue( ':class_page', 'Page', SQLITE3_TEXT );
+		$transaction->bindValue( ':class_blogpost', 'BlogPost', SQLITE3_TEXT );
 
 		$result = $transaction->execute();
 		if ( $result === false ) {
