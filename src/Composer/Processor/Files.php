55714cf31d9c1f31e05aa4a111bda29dfd98b464
Daniel Vogel <daniel.vogel@hallowelt.com>
2026-06-11 10:04:48 +0200

    Add contributor to default files

--- src/Composer/Processor/Files.php
+++ src/Composer/Processor/Files.php
@@ -88,7 +88,9 @@ class Files extends FileProcessorBase {
 					}
 
 					$attachmentContent = file_get_contents( $filePath );
-					$uploadFilePath = $this->workspace->saveUploadFile( "$timestamp-$filename", $attachmentContent, "result/images/$filename" );
+					$uploadFilePath = $this->workspace->saveUploadFile(
+						"$timestamp-$filename", $attachmentContent, "result/images/$filename"
+					);
 
 					// XML containing files is supported by MediaWiki dumpBackup but can not be imported
 					$this->builder->addFileRevision(
@@ -215,7 +217,7 @@ class Files extends FileProcessorBase {
 				$attachmentPageTitle,
 				$this->getRelativeFilePath( $uploadFilePath ),
 				'',
-				''
+				'Confluence-migration-tool'
 			);
 		}
 	}
