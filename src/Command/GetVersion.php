6d10e4ff0708349780208b5c551621f0624b30c9
Manuel Strehl <manuel.strehl@hallowelt.com>
2026-06-18 15:24:19 +0200

    re-sort use statements

--- src/Command/GetVersion.php
+++ src/Command/GetVersion.php
@@ -4,8 +4,8 @@ namespace HalloWelt\MigrateConfluence\Command;
 
 use HalloWelt\MigrateConfluence\Utility\Version;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GetVersion extends Command {
