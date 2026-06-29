62b3073cb4f81f86ddd57e5b79ef5550b6f35d4d
Manuel Strehl <manuel.strehl@hallowelt.com>
2026-06-29 10:21:54 +0200

    fine-tune DISPLAYTITLE and fix edge cases

--- src/Converter/Postprocessor/AddDisplayTitle.php
+++ src/Converter/Postprocessor/AddDisplayTitle.php
@@ -24,14 +24,17 @@ class AddDisplayTitle implements IPostprocessor {
 		/* strip, in this order, namespace and parent pages */
 		$baseWikiTitle = basename( preg_replace( '/[^:]*:/', '', $this->wikiTitle ) );
 
-		/* remove parent pages and replace any characters interfering with the wikisyntax below */
-		$baseConfluenceTitle = str_replace( [ '}', '|', '\\', ], '', basename( $this->confluencePageTitle ) );
+		/* replace the minimal set of characters interfering with the wikisyntax
+		 * below. Keep "foo/bar" structures intact, since that is allowed in
+		 * Confluence and we want to compare against a most authentic original
+		 * title. That's also why we cannot use TitleBuilder to sanitize it. */
+		$baseConfluenceTitle = str_replace( [ '}', '|', '\\', ], '', $this->confluencePageTitle );
 
 		if ( $baseWikiTitle !== str_replace( ' ', '_', $baseConfluenceTitle ) ) {
 			/* append and set the magic word with noreplace, so that a
 			 * potentially more specific title from an earlier processor can
 			 * override it */
-			$wikiText .= "\n{{DISPLAYTITLE:" . $baseConfluenceTitle . "|noreplace}}";
+			$wikiText .= "\n{{DISPLAYTITLE:" . htmlspecialchars( $baseConfluenceTitle ) . "|noreplace}}";
 		}
 		return $wikiText;
 	}
