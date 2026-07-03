ccda5b227afcd0c3921a6c291d6e7362d7f3cb18
driedmueller <daniel.riedmueller@hallowelt.com>
2026-07-03 07:42:33 +0200

    Code style

--- src/Command/Analyze.php
+++ src/Command/Analyze.php
@@ -20,8 +20,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Yaml\Exception\ParseException;
-use Symfony\Component\Yaml\Yaml;
 
 class Analyze extends BatchFileProcessorBase {
 
