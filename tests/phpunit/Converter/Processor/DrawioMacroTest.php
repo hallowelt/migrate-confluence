2e924e93d8d080ab13e300c6da4d8b46d5c4f073
Manuel Strehl <manuel.strehl@hallowelt.com>
2026-07-22 14:23:50 +0200

    fix PNGs for DrawIO not found
    ERM48553

--- tests/phpunit/Converter/Processor/DrawioMacroTest.php
+++ tests/phpunit/Converter/Processor/DrawioMacroTest.php
@@ -47,6 +47,74 @@ class DrawioMacroTest extends ProcessorTestCase {
 	}
 
 	/**
+	 * Regression test: when the macro's diagramName refers to a .drawio data file (not .png),
+	 * the converter must look up the corresponding .png by appending ".png" and bake the
+	 * diagram XML into that image's tEXt chunk.
+	 *
+	 * @covers HalloWelt\MigrateConfluence\Converter\Processor\DrawioMacro::process
+	 * @return void
+	 */
+	public function testBakesDiagramDataIntoImageWhenDiagramNameIsDataFile(): void {
+		$this->tempDir = sys_get_temp_dir() . '/confluence-migration-drawio-bake-test-' . uniqid();
+		mkdir( $this->tempDir, 0755, true );
+
+		$dataDir = dirname( __DIR__, 2 ) . '/Utility/DrawIOFileHandler/data';
+		$drawioDataFile = $dataDir . '/diagram.drawio';
+		$drawioPngFile  = $dataDir . '/diagram.drawio.png';
+
+		// Build a minimal workspace DB with one page containing both drawio files
+		$db = ( new WorkspaceDbMock() )->createWithDrawioAttachments(
+			1,
+			'DiagramPage',
+			'diagram.drawio',
+			$drawioDataFile,
+			'diagram.drawio.png',
+			$drawioPngFile
+		);
+
+		$writerDir = $this->tempDir . '/writer';
+		mkdir( $writerDir, 0755, true );
+		$conversionDataWriter = new ConversionDataWriter( $writerDir );
+		$dataLookup = new DBConversionDataLookup( $db );
+
+		$macroXml = <<<'XML'
+<xml xmlns:ac="some" xmlns:ri="thing">
+	<ac:structured-macro ac:name="drawio" ac:schema-version="1" ac:macro-id="test">
+		<ac:parameter ac:name="diagramName">diagram.drawio</ac:parameter>
+	</ac:structured-macro>
+</xml>
+XML;
+
+		$dom = new DOMDocument();
+		$dom->loadXML( $macroXml );
+
+		$processor = new DrawioMacro( $dataLookup, $conversionDataWriter, 1, 'DiagramPage' );
+		$processor->process( $dom );
+
+		// The PNG file must have been written with a tEXt chunk containing the diagram XML
+		// ConversionDataWriter::replaceConfluenceFileContent saves to $writerDir/images/$targetFilename
+		$writtenPng = $writerDir . '/images/DiagramPage-diagram.drawio.png';
+		$this->assertFileExists( $writtenPng, 'Baked PNG was not written' );
+
+		$pngContent = file_get_contents( $writtenPng );
+		$this->assertNotFalse( $pngContent );
+
+		// Count mxfile tEXt chunks
+		$chunks = 0;
+		// skip PNG signature
+		$i = 8;
+		while ( $i < strlen( $pngContent ) ) {
+			$length = unpack( 'N', substr( $pngContent, $i, 4 ) )[1];
+			$type = substr( $pngContent, $i + 4, 4 );
+			if ( $type === 'tEXt' ) {
+				$chunks++;
+			}
+			$i += 12 + $length;
+		}
+		$this->assertGreaterThanOrEqual( 1, $chunks, 'PNG must contain at least one mxfile tEXt chunk' );
+	}
+
+	/**
 	 * @param int $spaceId
 	 * @param string $input
 	 * @param string $output
