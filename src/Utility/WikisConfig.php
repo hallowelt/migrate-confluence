d2323ff95ce17108086ec45b985ac235a8943f13
Daniel Vogel <daniel.vogel@hallowelt.com>
2026-08-13 13:44:48 +0200

    Fix interwiki prefix

--- src/Utility/WikisConfig.php
+++ src/Utility/WikisConfig.php
@@ -51,7 +51,7 @@ class WikisConfig {
 	 * @return string
 	 */
 	public function getInterwikiPrefixForSpaceKey( string $spaceKey ): string {
-		$prefix = $this->getNamespaceForSpaceKey( $spaceKey );
+		$prefix = $this->getWikiNameForSpaceKey( $spaceKey );
 		return strtolower( "wiki-$prefix" );
 	}
 }
