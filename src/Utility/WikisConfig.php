30db133e83dcef9ce3122df92c9f4a6b5e3756bd
driedmueller <daniel.riedmueller@hallowelt.com>
2026-08-13 13:43:24 +0200

    fix empty wiki-name not working
    ERM49680

--- src/Utility/WikisConfig.php
+++ src/Utility/WikisConfig.php
@@ -51,7 +51,10 @@ class WikisConfig {
 	 * @return string
 	 */
 	public function getInterwikiPrefixForSpaceKey( string $spaceKey ): string {
-		$prefix = $this->getNamespaceForSpaceKey( $spaceKey );
+		$prefix = $this->getWikiNameForSpaceKey( $spaceKey );
+		if ( empty( $prefix ) ) {
+			$prefix = $this->getNamespaceForSpaceKey( $spaceKey );
+		}
 		return strtolower( "wiki-$prefix" );
 	}
 }
