067a7e7a95742a07cdb3809c270419d7a1eb8e16
Manuel Strehl <manuel.strehl@hallowelt.com>
2026-08-20 13:29:28 +0200

    use an attribute-like placeholder template

--- src/Utility/PlaceholderManager.php
+++ src/Utility/PlaceholderManager.php
@@ -5,7 +5,14 @@ namespace HalloWelt\MigrateConfluence\Utility;
 class PlaceholderManager {
 	private array $placeholders = [];
 
-	private const PLACEHOLDER_TEMPLATE = '___PLACEHOLDER_%d___';
+	/**
+	 * the placeholder template is chosen to look like an attribute, so that
+	 * pandoc keeps it even in situations where we are inside an element tag.
+	 *
+	 * The pseudo-namespace syntax is tested up untill pandoc 3.11 to produce
+	 * the expected result.
+	 */
+	private const PLACEHOLDER_TEMPLATE = 'convert:placeholder="%d"';
 
 	public function getPlaceholder( $string ): string {
 		if ( !isset( $this->placeholders[ $string ] ) ) {
