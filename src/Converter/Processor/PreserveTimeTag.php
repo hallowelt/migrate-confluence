eb67c6caa7032a30825d7ea2f93c4ff58e11af84
Manuel Strehl <manuel.strehl@hallowelt.com>
2026-09-11 14:09:54 +0200

    add support for content of <time> elements (#412)
    * add support for content of <time> elements
    
    ERM50163

--- src/Converter/Processor/PreserveTimeTag.php
+++ src/Converter/Processor/PreserveTimeTag.php
@@ -31,8 +31,23 @@ class PreserveTimeTag implements IProcessor, IUsesPlaceholder {
 
 		foreach ( $nonLiveList as $element ) {
 			$datetime = $element->getAttribute( 'datetime' );
+			/* preserve the tag's content, unless it is just a repeat of the datetime attribute */
+			$content = '';
+			foreach ( $element->childNodes as $child ) {
+				if ( $child->nodeType === XML_ELEMENT_NODE ) {
+					$content .= $dom->saveHTML( $child );
+				} elseif (
+						$child->nodeType === XML_TEXT_NODE &&
+						$child->nodeValue &&
+						$child->nodeValue !== $datetime ) {
+					$content .= $dom->saveHTML( $child );
+				}
+			}
+			if ( $content ) {
+				$content = " $content";
+			}
 			$replacement = $dom->createTextNode(
-				$this->placeholderManager->getPlaceholder( "<datetime>$datetime</datetime>" )
+				$this->placeholderManager->getPlaceholder( "<datetime>$datetime</datetime>$content" )
 			);
 
 			$element->parentNode->replaceChild( $replacement, $element );
